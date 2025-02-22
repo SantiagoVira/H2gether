5d73676308e54b4f49e3e98b47e5f060928a44c8
SantiagoVira <santiagovira06@gmail.com>
2025-02-22 13:03:45 -0500

    top of friends page

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -8,10 +8,29 @@ import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 
 export default function TabTwoScreen() {
+  const FRIENDS: { name: string; drank: number }[] = [
+    { name: "John B.", drank: 80 },
+    { name: "John C.", drank: 30 },
+    { name: "John D.", drank: 10 },
+    { name: "John E.", drank: 2 },
+  ];
+  const REQUESTS: { name: string; incoming: boolean }[] = [];
   return (
     <ThemedView style={styles.content}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Your Friends</ThemedText>
+        <ThemedView style={styles.titleTextContainer}>
+          <ThemedText type="title">Your Friends</ThemedText>
+          <ThemedText>
+            <ThemedText style={{ fontWeight: "bold" }}>
+              {FRIENDS.length}
+            </ThemedText>{" "}
+            friends •{" "}
+            <ThemedText style={{ fontWeight: "bold" }}>
+              {REQUESTS.length}
+            </ThemedText>{" "}
+            requests
+          </ThemedText>
+        </ThemedView>
         <Pressable
           onPress={() => {
             console.log("NEW FRIEND");
@@ -23,94 +42,10 @@ export default function TabTwoScreen() {
           />
         </Pressable>
       </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
+
+      <ThemedText type="title" style={{ marginTop: 36 }}>
+        Leaderboard
       </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ alignSelf: "center" }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-          to see how to load{" "}
-          <ThemedText style={{ fontFamily: "SpaceMono" }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
     </ThemedView>
   );
 }
@@ -125,9 +60,12 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "space-between",
-    gap: 8,
+  },
+  titleTextContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
   },
   noProjectsWrapper: {
     alignItems: "center",
