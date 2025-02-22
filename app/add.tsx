6e01dd95e99bd647718439d911f6676ace6bb5f4
SantiagoVira <santiagovira06@gmail.com>
2025-02-22 13:18:42 -0500

    Header for add page (bad)

--- app/add.tsx
+++ app/add.tsx
@@ -6,26 +6,49 @@ import { ThemedView } from "@/components/ThemedView";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
+    <ThemedView style={styles.content}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Drank some water?</ThemedText>
       </ThemedView>
-    </>
+      <ThemedText>Awesome! Let's track it!</ThemedText>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: "hidden",
+    paddingTop: 48,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  noProjectsWrapper: {
     alignItems: "center",
     justifyContent: "center",
-    padding: 20,
+    flex: 1,
+    width: "100%",
+    padding: "10%",
+    marginBottom: 86,
+    gap: 8,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  noProjectsText: {
+    fontSize: 32,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  grid: {
+    marginBottom: 52,
+  },
+  footer: {
+    position: "absolute",
+    right: 12,
   },
 });
